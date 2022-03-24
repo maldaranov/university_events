97a2ec9cc025c300f9af3da0f933564f8ee7d65d
Maxim Aldaranov <maxim.aldaranov@gmail.com>
2022-03-23 23:55:30 -0400

    refresh

--- create_event.php
+++ create_event.php
@@ -6,7 +6,11 @@
     <h1> Create Event </h1>
 
     <form action="includes/create_event-inc.php" method="post">
-            <input type="text" name="eventName" placeholder="name">
+            <input type="text" size=30 name="eventName" placeholder="name">
+            <input type="text" name="type" placeholder="type">
+            <textarea name="description" rows=8 cols=30 wrap=virtual></textarea>
+            <input type="text" name="category" placeholder="category">
+            <input type="datetime" name="datetime" placeholder="date and time">
             <button type="submit" name="submit">Create event</button>
     </form>
 </div>
