1388aab04ada6f7c55e2685d747bbf2ed2a84b02
Maxim Aldaranov <maxim.aldaranov@gmail.com>
2022-03-23 23:45:12 -0400

    refresh

--- create_event.php
+++ create_event.php
@@ -9,7 +9,7 @@
             <input type="text" size=30 name="eventName" placeholder="name">
             <input type="text" name="type" placeholder="type">
             <textarea name="description" rows=8 cols=30 wrap=virtual></textarea>
-            <input type="text" name="category" placeholder="cateegory">
+            <input type="text" name="category" placeholder="category">
             <input type="datetime" name="datetime" placeholder="date and time">
             <button type="submit" name="submit">Create event</button>
     </form>
