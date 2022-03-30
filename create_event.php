00d543f44d959c967808fda52d939a8457c00a0b
Maxim Aldaranov <maxim.aldaranov@gmail.com>
2022-03-30 17:55:57 -0400

    create-event.php update

--- create_event.php
+++ create_event.php
@@ -10,9 +10,13 @@
             <label for="eName"> Event name: </label>
             <input id="eName" type="text" required size=30 name="eventName" placeholder="name">
 
+            <!-- event location input-->
+            <label for="eLocation"> Location: </label>
+            <textarea id="eLocation" name="eventDescription" rows=5 cols=27 maxlength=500 placeholder="address"></textarea>
+
             <!-- event category input -->
-            <label for="eventCategory"> Category:
-            <select id="eventCategory" name="eventCategory">
+            <label for="eCategory"> Category:
+            <select id="eCategory" name="eventCategory">
                 <option value=""> Please choose a category </option>
                 <option value="social"> social </option>
                 <option value="fundraising"> fundraising </option>
@@ -20,22 +24,36 @@
                 <option value="promotional"> promotional </option>
             </select>
             
-            <!-- event date and time input -->
-            <label for="eventDatetime"> Date and Time: </label>
-            <input id="eventDatetime" type="datetime-local" name="eventDatetime" placeholder="date and time">
+            <!-- event date input -->
+            <label for="eDate"> Date: </label>
+            <input id="eDate" type="date" name="eventDate" placeholder="date">
 
-            <!-- event privacy -->
-            <label for="eventPRivacy"> Type:
-            <select id="eventPrivacy" name="eventPrivacy">
-                <option value=""> Please choos a type </option>
-                <option value="public"> public </option>
-                <option value="private"> private </option>
-                <option value="rso"> rso </option>
+            <!-- event time slot input -->
+            <label for="eTime"> Time slot: </label>
+            <select id="eTime" name="eventTime">
+                <option value=""> Please choose a time slot </option>
+                <option value="1"> 9:00 a.m - 10:00 a.m </option>
+                <option value="2"> 10:00 a.m - 11:00 a.m </option>
+                <option value="3"> 11:00 a.m - 12 p.m </option>
+                <option value="4"> 12 p.m - 1:00 p.m </option>
+                <option value="5"> 1:00 p.m - 2:00 p.m </option>
+                <option value="6"> 2:00 p.m - 3:00 p.m </option>
+                <option value="7"> 3:00 p.m - 4:00 p.m </option>
+                <option value="8"> 4:00 p.m - 5:00 p.m </option>
+                <option value="9"> 5:00 p.m - 6:00 p.m </option>
             </select>
 
-            <!-- event description-->
-            <label for="eventDescription"> Description: </label>
-            <textarea id="eventDescription" name="eventDescription" rows=10 cols=50 maxlength=500 placeholder="maximum length is 500 characters"></textarea>
+            <!-- event privacy input -->
+            <label for="ePrivacy"> Type:
+            <select id="ePrivacy" name="eventPrivacy">
+                <option value=""> Please choose a type </option>
+                <option value="public"> public </option>
+                <option value="rso"> private (RSO) </option>
+            </select>
+
+            <!-- event description input -->
+            <label for="eDescription"> Description: </label>
+            <textarea id="eDescription" name="eventDescription" rows=10 cols=27 maxlength=500 placeholder="maximum length is 500 characters"></textarea>
             <p><button type="submit" name="submit">HOST</button></p>
     </form>
 </div>
