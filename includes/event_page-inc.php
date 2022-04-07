cddc305f85e6dc6616016c7b9d4bb43bd3df3fd0
chanxayb <chanxayb@gmail.com>
2022-04-06 20:36:24 -0400

    Replaced hardcoded values
    Next step is edit comment functionality

--- includes/event_page-inc.php
+++ includes/event_page-inc.php
@@ -7,13 +7,12 @@ function setComments($db)
         require 'server.php'; // grab the database
         
         $comment_eventId = $_POST['comment_eventId'];
-        $comment_userId = $_POST['comment_userId'];
         $comment_userName = $_POST['comment_userName'];
         $comment_msg = $_POST['comment_msg'];
         $comment_datetime = $_POST['comment_datetime'];
     
-        $sql = "INSERT INTO event_comment (comment_eventId, comment_userId, comment_userName, comment_msg, comment_datetime) 
-                VALUES ('$comment_eventId', '$comment_userId', '$comment_userName', '$comment_msg', '$comment_datetime')";
+        $sql = "INSERT INTO event_comment (comment_eventId, comment_userName, comment_msg, comment_datetime) 
+                VALUES ('$comment_eventId', '$comment_userName', '$comment_msg', '$comment_datetime')";
         
         $result = $db->query($sql);
     }
